255fe921be54468b18803d99fe3fd3d81347c67f
jorge <jorge.carrasco@ideeo.mx>
2017-06-26 13:02:37 -0500

    archivos

---
+++ index.php
@@ -0,0 +1,59 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+    <title>Just Win</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style-jor.css" rel="stylesheet">
+</head>
+
+<div class="container-fluid">
+	<div class="row">
+	 <br>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="index.php"><img src="images/logo.png"></a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a class="login" href="#">¿CÓMO FUNCIONA?</a></li>
+						<li><a class="login login-button" href="#">LOGIN</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<img src="images/coin.png">
+		</div>
+	
+
+		<div class="col-md-6 col-l">
+			<h1>Gana dinero de la forma más fácil con JustWIN.</h1>
+			<p>Gana dinero de la forma más fácil con JustWIN.</p>
+			<a class="registro" href="#">REGISTRARME</a>
+		</div>
+		</div>
+
+</div>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
