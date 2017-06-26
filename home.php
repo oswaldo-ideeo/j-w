f329a1d1bb754cd40a91f616e03d82386363f348
oswaldo-ideeo <oswaldo.castaneda@ideeo.mx>
2017-06-26 13:17:59 -0500

    mas archivos

---
+++ home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<?php include 'includes/head.php' ?>
+</head>
+<body>
+	
+</body>
+</html>
--- includes/head.php
+++ includes/head.php
@@ -1,1 +1,9 @@
-head.php
+<title><?= $title ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, minimum-scale=1"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/font-awesome.min.css">
+    <link href="css/style-jor.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/css.css">
