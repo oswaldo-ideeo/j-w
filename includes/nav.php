f64e5c459e923a610977e84e81f969967562d8f1
oswaldo-ideeo <oswaldo.castaneda@ideeo.mx>
2017-06-26 20:22:05 -0500

    navegacion

--- includes/nav.php
+++ includes/nav.php
@@ -2,19 +2,19 @@
 		<div class="row" class="nav">
 			<div class="col-sm-3 navegador">
 				<div class="logo-container">
-					<img src="images/logo-min.png" alt="">
+					<a href="home.php"><img src="images/logo-min.png" alt=""></a>
 				</div>
 			</div>
 			<div class="col-sm-6 navegador menu">
 				<ul class="text-azul">
-					<li class="menu-item <?= $inicio ?>">INICIO</li>
-					<li class="menu-item <?= $actividades ?>">ACTIVIDADES</li>
-					<li class="menu-item <?= $niveles ?>">NIVELES</li>
+					<a href="home.php"><li class="menu-item <?= $inicio ?>">INICIO</li></a>
+					<a href="actividades.php"><li class="menu-item <?= $actividades ?>">ACTIVIDADES</li></a>
+					<a href="niveles.php"><li class="menu-item <?= $niveles ?>">NIVELES</li></a>
 				</ul>
 			</div>
 			<div class="col-sm-3 navegador t-right">
-				<div class="fa fa-bell-o usuario alin"></div>
-				<p class="n-usuario usuario alin text-rojo">Alex Robinson</p>
+				<a href="notificaciones.php"><div class="fa fa-bell-o usuario alin"></div></a>
+				<a href="perfil.php"><p class="n-usuario usuario alin text-rojo">Alex Robinson</p></a>
 				<div class="f-usuario usuario"><img src="images/user-example.png" alt=""></div>
 				<div class="fa fa-chevron-down usuario alin"></div>
 			</div>
