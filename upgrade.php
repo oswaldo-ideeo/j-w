dc66b68e843117362e9d1326b3492f9e3176710d
jorge <jorge.carrasco@ideeo.mx>
2017-06-26 19:33:42 -0500

    sd

--- upgrade.php
+++ upgrade.php
@@ -23,7 +23,7 @@
 							<div class="col-md-2">
 								<img class="upgrade" src="images/foto-perfil.png">
 							</div>
-							<div class="col-md-2">
+							<div class="col-md-2 necesitams">
 								<p>Necesitamos una fotografía para acreditar la titularidad de la Tarjeta.
 									<b class="term">Ver ejemplo.</b></p>
 								</div>
@@ -38,10 +38,10 @@
 							<div class="container">
 								<div class="row">
 									<div class="col-md-6 cards-upgrade">
-										<div class="col-md-1"><img src="images/visa.png"></div>
-										<div class="col-md-1"><img src="images/mastercard.png"></div>
-										<div class="col-md-1"><img src="images/american.png"></div>
-										<div class="col-md-1"><img src="images/bva.png"></div>
+										<div class="col-xs-1 col-md-1"><img src="images/visa.png"></div>
+										<div class="col-xs-1 col-md-1"><img src="images/mastercard.png"></div>
+										<div class="col-xs-1 col-md-1"><img src="images/american.png"></div>
+										<div class="col-xs-1 col-md-1"><img src="images/bva.png"></div>
 									</div>
 								</div>
 							</div>
@@ -56,12 +56,12 @@
 
 							<div class="container">
 								<div class="row">
-									<div class="col-md-9 col-xs-12  cards-upgrade-second">
-										<div class="col-md-1"><input class="input-card" type="text" placeholder="MM" name=""></div>
-										<div class="col-md-1"><input class="input-card" type="text" placeholder="YY" name=""></div>
-										<div class="col-md-1"><p class="text-cvv"><b>CVV</b></p></div>
-										<div class="col-md-1"><input class="input-card" type="text" placeholder="MM" name=""></div>
-										<div class="col-md-1"><img src="images/question.png"></div>
+									<div class="col-md-6 col-lg-9  cards-upgrade-second">
+										<div class="col-xs-1 col-md-1 float"><input class="input-card" type="text" placeholder="MM" name=""></div>
+										<div class="col-xs-1 col-md-1 float"><input class="input-card" type="text" placeholder="YY" name=""></div>
+										<div class="col-xs-1 col-md-1 float"><p class="text-cvv"><b>CVV</b></p></div>
+										<div class="col-xs-1 col-md-1 float"><input class="input-card" type="text" placeholder="MM" name=""></div>
+										<div class="col-xs-1 col-md-1 float"><img src="images/question.png"></div>
 									</div>
 								</div>
 
